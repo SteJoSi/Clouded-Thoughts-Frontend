324d05ce293a7900b5a3cc8c17af984033ae2234
Stephanie Sison <sjsison27@icloud.com>
2023-01-17 09:38:23 -0800

    add icon to navbar

--- clouded-thoughts-frontend/src/components/NavBar.js
+++ clouded-thoughts-frontend/src/components/NavBar.js
@@ -16,7 +16,7 @@ function NavBar() {
             <nav id="navbar">
                 <Link exact to="/" style={navStyles}><ion-icon name="home"></ion-icon> Home</Link>
                 <Link exact to="/users" style={navStyles}><ion-icon name="person"></ion-icon>Users</Link>
-                <Link exact to="/posts" style={navStyles}><ion-icon name="posts"></ion-icon>Posts</Link>
+                <Link exact to="/posts" style={navStyles}><ion-icon name="reader"></ion-icon>Posts</Link>
                 <Link exact to="/newEntry" style={navStyles}><ion-icon name="create"></ion-icon>New Entry</Link>
                 {/* <Link exact to="/logout" style={navStyles}><ion-icon name="walk"></ion-icon>Sign Out</Link> */}
             </nav>
