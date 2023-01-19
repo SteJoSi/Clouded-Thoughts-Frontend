9b55862531767c0667e8d34375fe7b1e3bc75557
Stephanie Sison <sjsison27@icloud.com>
2023-01-18 20:46:42 -0800

    make a link in userContainer to post component

--- clouded-thoughts-frontend/src/containers/UserContainer.js
+++ clouded-thoughts-frontend/src/containers/UserContainer.js
@@ -3,7 +3,7 @@ import React from 'react'
 function UserContainer({ users }) {
   return (
     <div id="userContainer" key={users.id}>
-      <h3 className="username"><ion-icon name="person"></ion-icon>{users.username}</h3>
+      <a className="username" href="http://localhost:3000/posts"><ion-icon name="person"></ion-icon>{users.username}</a>
     </div>
   )
 }
